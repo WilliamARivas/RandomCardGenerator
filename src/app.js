4e7b041143335147f43fc46ba3a59f2b5171812b
WilliamARivas <willarivas@yahoo.com>
2022-09-17 01:48:36 +0000

    taking a break tonight

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.body.addEventListener("load", event => {
+  let numberField = document.querySelector("h1");
+  numberField.innerHTML = randomNumber();
+});
+
+let myCardArray = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+function randomNumber() {
+  let newNum = Math.floor(Math.random() * (myCardArray.length + 1));
+  return myCardArray[newNum];
+}
